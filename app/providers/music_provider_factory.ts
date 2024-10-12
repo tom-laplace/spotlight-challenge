2aaacd9bb7d12d403e6ac33b3a4da403f42f060b
Tom <tomlaplace0902@gmail.com>
2024-10-12 14:25:42 +0200

    feat : add spotify provider

---
+++ app/providers/music_provider_factory.ts
@@ -0,0 +1,28 @@
+import { SpotifyArtist } from '../types/spotify_api/spotify_artist.js'
+import { SpotifyProvider } from './spotify_provider.js'
+
+export interface MusicProvider {
+  getUserTopArtists(limit: number): Promise<SpotifyArtist[]>
+}
+
+export enum MusicService {
+  SPOTIFY = 'spotify',
+}
+
+export class MusicProviderFactory {
+  private static providers: Map<MusicService, MusicProvider> = new Map()
+
+  static getProvider(service: MusicService, authToken: string): MusicProvider {
+    if (!this.providers.has(service)) {
+      switch (service) {
+        case MusicService.SPOTIFY:
+          this.providers.set(service, new SpotifyProvider(authToken))
+          break
+        default:
+          throw new Error('Invalid music service')
+      }
+    }
+
+    return this.providers.get(service)!
+  }
+}
